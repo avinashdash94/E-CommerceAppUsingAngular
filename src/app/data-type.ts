deb993c76f821472b9c85a8e60d45a0b8d7a77e2
alnu <20avinashdash@gmail.com>
2023-09-07 17:33:44 +0530

    added new component to add new product and for the same addd service to addd into json api for that added new item in db.json ie: products

--- src/app/data-type.ts
+++ src/app/data-type.ts
@@ -10,3 +10,12 @@ export interface login {
     email: string,
     password: string
 }
+
+export interface product{
+    name: string,
+    price: number,
+    category: string,
+    color:string,
+    description: string,
+    image:string
+}
--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerAddProductComponent implements OnInit {
 
-  constructor() { }
+  addProductMessage: string|undefined;
+  constructor(private product:ProductService) { }
 
   ngOnInit(): void {
   }
-
+  
+  submit(data:product){
+    // console.log(data);
+    this.product.addProduct(data).subscribe((result) =>{
+      // console.log(result);
+      this.addProductMessage="Product is successfully added";
+    });
+    setTimeout(()=> this.addProductMessage=undefined, 3000);
+  }
 }
