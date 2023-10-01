bd18510ff7d44b3b5ffd23a5bf095de0caa4e8cb
alnu <20avinashdash@gmail.com>
2023-10-01 17:31:54 +0530

    added functionality to pre fill the record which we want to update and update the new data on submit

--- src/app/seller-update-product/seller-update-product.component.ts
+++ src/app/seller-update-product/seller-update-product.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 @Component({
   selector: 'app-seller-update-product',
@@ -7,12 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerUpdateProductComponent implements OnInit {
 
-  constructor() { }
+  productData: undefined | product;
+  productMessage: undefined | string;
+  constructor(private route: ActivatedRoute, private product: ProductService ) { }
 
   ngOnInit(): void {
+    let productId = this.route.snapshot.paramMap.get('id');
+    // console.log(productId);
+    productId && this.product.getProduct(productId).subscribe((data)=>{
+      //console.log(data);
+      this.productData = data;
+    })
   }
-  submit(data){
-    
+  submit(data:product){
+    // console.log(data);t
+    if(this.productData) {
+      data.id = this.productData.id;
+    }
+    this.product.updateProduct(data).subscribe((result) => {
+      if(result){
+        this.productMessage = "Prodcut has been updated";
+      }
+    });
+    setTimeout(()=> this.productMessage = undefined, 3000);
   }
 
 }
